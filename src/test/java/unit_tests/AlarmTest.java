5ae0d0232c92561c8b4ec4c5b6b322ed8cfefc3f
trikitrok <trikitrok@gmail.com>
2017-06-21 01:18:47 +0200

    initial commit

---
+++ src/test/java/unit_tests/AlarmTest.java
@@ -0,0 +1,15 @@
+package unit_tests;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class AlarmTest {
+
+    @Test
+    public void deleteMe() {
+        assertThat(true, is(false));
+    }
+}
+
